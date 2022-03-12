4a0512501eaf887c2b44b25acabc6356b43a8ddc
Jo Thomas <jothomas1996@gmail.com>
2022-03-12 11:59:08 +0530

    added data cleaning

--- main.py
+++ main.py
@@ -6,33 +6,43 @@ try:
 except Exception as e:
 	data_file = open("data.in", "r", encoding="utf-8")
 
-content = data_file.read()
+input_data = data_file.read()
 
 data_file.close()
 
 # --------------------------------------------------------------------
 
-contacts = []
-count = 0
+
+data = []
 temp = ""
+input_data_length = len(input_data)
 
-print("File length: ", len(content))
+print("Data length: ", input_data_length)
 
-for character in content:
-	count += 1
-	print(count, " characters scanned", end="\r")
+for i in range(len(input_data)):
+	print(i + 1, "characters scanned - ", ((i + 1)/input_data_length) * 100, '%',  end="\r")
 
-	if(character.isdigit() or character == '+' or character == ' '):
-		temp += character
+	if(input_data[i].isdigit() or input_data[i] == '+' or input_data[i] == ' '):
+		temp += input_data[i]
 	elif(temp.strip() != ""):
-		contacts.append(temp.strip())
+		data.append(temp.strip())
 		temp = ""
 
-print("\n\n\b", len(contacts), " concats found")
+print("\n\n\n", len(data), "values found")
 
 # --------------------------------------------------------------------
 
+# clean data
+output_data = []
 count = 0
+
+for i in range(len(data)):
+	if(len(data[i]) >= 10 and data[i][0] == '+' and data[i] not in output_data):
+		output_data.append(data[i])
+		count += 1
+		print(count, "contacts retrieved", end="\r")
+
+# --------------------------------------------------------------------
 
 # set output data file
 try:
@@ -40,11 +50,9 @@ try:
 except Exception as e:
 	data_file = open("data.out", "w")
 
-for contact in contacts:
-	if(len(contact) >= 10):
-		count += 1
-		print(count, " contacts saved", end="\r")
-		data_file.write(contact)
-		data_file.write("\n")
+for i in range(len(output_data)):
+	print(i + 1, "contacts saved", end="\r")
+	data_file.write(output_data[i])
+	data_file.write("\n")
 
 data_file.close()
